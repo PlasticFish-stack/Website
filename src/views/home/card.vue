d884cf5e717d5aa48066051e52a89f421571c653
Plastic <plasticfish@foxmail.com>
2023-06-06 00:28:30 +0800

    update

--- src/views/home/card.vue
+++ src/views/home/card.vue
@@ -1,16 +1,37 @@
 <script setup>
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger'
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
+import { onMounted } from 'vue';
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin)
 
+onMounted(() => {
+  gsap.fromTo(window, {
+    scrollTrigger: {
+      trigger: "#flow",
+      start: "top center",
+      end: "top top",
+      scrub: 1,
+      once: true
+    },
+    scrollTo: {y: "#check",  },
+    duration: 1
+  })
+})
 </script>
 
 <template>
   <div id="main">
     <div id="flow">
-      (*Φ皿Φ*)123
+      <span id="flow_top">Products</span>
+      <span id="flow_center">产品中心</span>
+      <span id="flow_bottom">系列展示</span>
     </div>
     <div id="layout">
       <div>SSP舞台灯光</div>
       <div>ARC建筑照明</div>
     </div>
+    <div id="check"></div>
   </div>
 
 </template>
@@ -20,13 +41,39 @@
   width: 100%;
   display: flex;
   color: red;
-  justify-content: center;
+  background-color: #fffef9;
+  flex-direction: column;
+  align-items: center;
   flex-wrap: wrap;
   #flow{
+    height: 150px;
+    margin-top: 50px;
+    margin-bottom: 50px;
     width: 1280px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: #2b3138;
+    #flow_top{
+      font-size: 50px;
+      text-transform: uppercase;
+    }
+    #flow_center{
+      margin-top: 20px;
+      font-size: 40px;
+      -webkit-text-stroke: black 0.7px;
+    }
+    #flow_bottom{
+      margin-top: 14px;
+      font-size: 25px;
+      letter-spacing: 18px;
+      direction: unset;
+      padding-left: 18PX;
+      -webkit-text-stroke: black 0.5px;
+    }
   }
   #layout{
-    height: 100vh;
+    height: calc(100vh - 150px);
     width: 1280px;
     background-color: black;
   }
