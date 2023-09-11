5873a46df960f538f1b518e5ba0084691600ac67
Plastic <plasticfish@foxmail.com>
2023-09-12 00:46:31 +0800

    update

--- src/views/home/card.vue
+++ src/views/home/card.vue
@@ -33,7 +33,7 @@ onMounted(() => {
       return initBox.xEnd - initBox.xStart + initBox.width + margin
     },
     snap: gsap.utils.snap(1),
-    draggableStart : null,
+    draggableStart: null,
     draggableEnd: null
   };
   let times = [];
@@ -60,9 +60,9 @@ onMounted(() => {
   })
   loop.progress(1, true).progress(0, true);
   // let ratio = gsap.utils.snap((1 / box.length ));;
-  
-  const rtaio_i = (1/initBox.totalWidth())*30
-  const curStart = ((document.querySelector('#layout_box').offsetWidth / 2) - (initBox.width / 2))   +  ((-box[0].offsetLeft) + initBox.width);
+
+  const rtaio_i = (1 / initBox.totalWidth()) * 30
+  const curStart = ((document.querySelector('#layout_box').offsetWidth / 2) - (initBox.width / 2)) + ((-box[0].offsetLeft) + initBox.width);
 
 
   const timeWrap = gsap.utils.wrap(0, loop.duration());
@@ -71,16 +71,16 @@ onMounted(() => {
   })
   loop.seek(times[0])
   const wrap = gsap.utils.wrap(0, 1);
-  console.log(loop.progress());
+
   let io = new Array(8);
-  console.log();
-  for(let i = 0; i < box.length; i++){
-    io[i] = wrap(loop.progress() + (1/initBox.totalWidth() * 1310)*i)
-    console.log(io[i], );
+
+  for (let i = 0; i < box.length; i++) {
+    io[i] = wrap(loop.progress() + (1 / initBox.totalWidth() * 1310) * i)
+
   }
-  console.log(io);
+
   const ratio = gsap.utils.snap(io)
-  
+
   const newArr = [];
   const loopProgress = loop.progress();
   times.forEach((item, index) => {
@@ -98,15 +98,14 @@ onMounted(() => {
     onDrag() {
       loop.progress(wrap(initBox.draggableStart + (this.startX - this.x) * (1 / initBox.totalWidth())));
       initBox.draggableEnd = loop.progress();
-      console.log(loop.progress());
+
     },
     onDragEnd() {
-      animaDraggable = ratio(wrap(initBox.draggableEnd)) ;
-      console.log(animaDraggable - rtaio_i /2, 'ani');
+      animaDraggable = ratio(wrap(initBox.draggableEnd));
       gsap.fromTo(loop, {
         progress: initBox.draggableEnd
       }, {
-        progress: (animaDraggable - rtaio_i/2 ),
+        progress: animaDraggable,
         ease: "back.out(1.1)",
         duration: 0.4
       })
@@ -115,151 +114,26 @@ onMounted(() => {
   loop.draggable = draggable;
 
 
-//test
-  console.log(1 / box.length);
+  //test
+  let config= {};
+  let time = null;
+  let nowIndex = 0;
+  box.forEach((item, index) => {
+    item.addEventListener('click', () => {
+      time = times[index];
+      console.log({'time': time, 'now': loop.time() , 'index' : index, 'nowindex': nowIndex});
+      if(time < loop.time() && index > nowIndex){
+        time += loop.duration();
+        console.log('chus');
+      }
+      nowIndex = index
+      config.duration = 1
+      loop.tweenTo(time, config)
+    })
+  })
 
 
 
-
-
-
-
-
-
-
-
-  // const boxes = gsap.utils.toArray('.swiper_box');
-  // const boxes_parentNode = boxes[0].parentNode;
-  // console.log(boxes_parentNode);
-  // gsap.set(boxes, {
-  //   marginRight: 20
-  // })
-  // let loop = gsap.timeline({
-  //   paused: true,
-  //   repeat: -1,
-  //   defaults: { ease: 'none' },
-  //   onReverseComplete: () => loop.totalTime(loop.rawTime() + loop.duration() * 100)
-  // });
-
-
-  // const pixelSpeed = 1 * 100;
-  // const snap = gsap.utils.snap(1);
-
-  // const startX = boxes[0].offsetLeft;
-  // let widths = [],
-  //   xPercents = [];
-  // function populateWdith() {
-  //   boxes.forEach((el, index) => {
-  //     widths[index] = parseFloat(gsap.getProperty(el, 'width', 'px'));
-  //     xPercents[index] = snap(parseFloat(gsap.getProperty(el, 'x', 'px')) / widths[index] * 100 + gsap.getProperty(el, 'xPercent'))
-  //   })
-  // }
-  // populateWdith();
-  // const totalWidth = computed(() => {
-  //   if (boxes[0].offsetLeft < 0) {
-  //     return Math.abs(boxes[0].offsetLeft) + Math.abs(boxes[boxes.length - 1].offsetLeft) + gsap.getProperty(boxes[boxes.length - 1], "width");
-  //   } else {
-  //     return Math.abs(boxes[boxes.length - 1].offsetLeft) + gsap.getProperty(boxes[boxes.length - 1], "width");
-  //   }
-  // })//总宽度
-  // const times = [];
-  // boxes.forEach((el, index) => {
-  //   const distanceToStart = el.offsetLeft - startX;
-  //   console.log(el.offsetLeft, el.innerHTML);
-  //   console.log(distanceToStart);
-  //   const distanceToLoop = distanceToStart + widths[index];
-  //   loop.to(el, {
-  //     xPercent: snap((-distanceToLoop) / widths[index] * 100),
-  //     duration: distanceToLoop / pixelSpeed
-  //   }, 0)
-  //   loop.fromTo(el, {
-  //     xPercent: snap(((totalWidth.value - distanceToLoop + gsap.getProperty('.swiper_box', 'marginRight')) / widths[index] * 100))
-  //   }, {
-  //     xPercent: 0,
-  //     duration: (totalWidth.value - distanceToLoop + gsap.getProperty('.swiper_box', 'marginRight')) / pixelSpeed,
-  //     immediateRender: false
-  //   }, distanceToLoop / pixelSpeed)
-  //     .add("label" + index, distanceToStart / pixelSpeed)
-  //   times[index] = distanceToStart / pixelSpeed
-  // });
-  // const wrap = gsap.utils.wrap(0, 1);
-  // const ratio = 1 / totalWidth.value;
-  // const dragSnap = totalWidth.value / boxes.length;
-
-  // let startProgress = null;
-  // let snap_sec = gsap.utils.snap(1 / boxes.length);
-  // let transform_start = null;
-  // let transform_end = null;
-  // console.log(loop.duration() );
-  // let timeOffset = loop.duration() * (boxes_parentNode.offsetWidth /2) / totalWidth.value;
-  // console.log(timeOffset, 'off');
-
-
-
-  // loop.progress(1, true).progress(0, true);
-  // const scope = gsap.utils.wrap(0, 7)
-
-
-  // const draggable = Draggable.create('.drag-proxy', {
-  //   trigger: '#layout_box',
-  //   type: "x",
-  //   onPress() {
-  //     startProgress = loop.progress();
-  //     loop.progress(startProgress);
-  //   },
-  //   onDrag() {
-  //     loop.progress(wrap(startProgress + (this.startX - this.x) * ratio));
-  //     transform_start = loop.progress()
-  //   },
-  //   onRelease() {
-  //     transform_end = snap_sec(loop.progress());
-  //     gsap.fromTo(loop, {
-  //       progress: transform_start
-  //     }, {
-  //       progress: transform_end,
-  //       ease: "back.out(1.1)",
-  //       duration: 0.75
-  //     })
-  //   },
-  //   onThrowUpdate() {
-  //     loop.progress(wrap(startProgress + (this.startX - this.x) * ratio));
-  //     transform_start = loop.progress()
-  //   },
-  // })
-  // console.log(loop);
-  // loop.draggable = draggable;
-  // loop.times = times
-  // let test = 0;
-  // const test_t = gsap.utils.wrap(0, 80)
-  // const toIndex = (index, vars) => {
-  //   vars = vars || {};
-  //   (Math.abs(index - num) > boxes.length / 2) && (index += index > num ? -boxes.length : boxes.length);
-  //   let newIndex = gsap.utils.wrap(0, boxes.length, index);
-  //   let time = times[newIndex];
-  //   console.log(time, loop.time(), index , num);
-  //   if (time > loop.time() !== index > num) {
-  //     vars.modifiers = {time: gsap.utils.wrap(0, loop.duration())}
-  //     time += loop.duration() * (index > num ? 1 : -1);
-  //   }
-  //   num = newIndex;
-  //   // return loop.tweenTo(('label' + (newIndex)).toString(), {
-  //   //   duration: 0.4,
-  //   //   overwrite: true
-  //   // })
-
-  //   return loop.tweenTo(time, vars)
-  // }
-  // const prev = document.querySelector('#prev').addEventListener('click', () => {
-  //   toIndex(num - 1, {duration: 0.4})
-  // })
-  // const next = document.querySelector('#next').addEventListener('click', () => {
-  //   toIndex(num + 1)
-  // })
-  // boxes.forEach((item, index) => {
-  //   item.addEventListener('click', () => {
-
-  //   })
-  // })
 
 
 
@@ -274,7 +148,7 @@ onMounted(() => {
 
 <template>
   <div id="main">
-    
+
     <div id="flow">
       <span id="flow_top">视频中心</span>
       <span id="flow_center">VIDEO CENTER</span>
@@ -286,10 +160,10 @@ onMounted(() => {
         <div style="border: 1px solid red;">
           {{ item }}
         </div>
-        
+
       </div>
       <div>
-<!--         
+        <!--         
         <div id="prev">左</div>
         <div id="next">右</div> -->
       </div>
